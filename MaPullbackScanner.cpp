bc779376e973d140ee3806db3831a6a768661e6e
asd977 <61139971+asd977@users.noreply.github.com>
2026-01-20 18:59:44 +0800

    Relax pullback filter to allow below MA

--- MaPullbackScanner.cpp
+++ MaPullbackScanner.cpp
@@ -211,7 +211,7 @@ void MaPullbackScanner::pumpKline()
         if (cacheGet(secid, dates, closes) && closes.size() >= need) {
             PullbackKlineStats st;
             if (computeStatsFromBars(dates, closes, m_cfg.maPeriod, m_cfg.belowDays, st) && st.ok) {
-                if (s.last > st.maLast && st.lastNDaysCloseBelowMA) {
+                if (st.lastNDaysCloseBelowMA) {
                     PullbackRow r;
                     r.code = s.code; r.name = s.name; r.market = s.market;
                     r.sector = s.sector; r.pe = s.pe;
@@ -353,7 +353,7 @@ void MaPullbackScanner::sendKlineTask(Task t)
 
         PullbackKlineStats st;
         if (computeStatsFromBars(dates, closes, m_cfg.maPeriod, m_cfg.belowDays, st) && st.ok) {
-            if (t.s.last > st.maLast && st.lastNDaysCloseBelowMA) {
+            if (st.lastNDaysCloseBelowMA) {
                 PullbackRow r;
                 r.code = t.s.code; r.name = t.s.name; r.market = t.s.market;
                 r.sector = t.s.sector; r.pe = t.s.pe;
